e34a28f68a711461de0c2386f6cf122ced761827
Udee-y <udemeinyang920@gmail.com>
2022-06-25 23:09:35 +0100

    updated

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     scores[activePlayer];
 
   //Check if the player won (first player that got to 100)
-  if (scores[activePlayer] >= 10) {
+  if (scores[activePlayer] >= 100) {
     document.querySelector("#name-" + activePlayer).textContent = "WINNER!";
     document.querySelector(".dice").style.display = "none";
     document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
